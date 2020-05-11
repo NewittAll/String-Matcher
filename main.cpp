4d61c87a01c286f9f43f1fdd96a70bba37fc9ea6
Ryan <Newitt@gmail.com>
2020-05-11 17:05:16 -0400

    Learning output and strings

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    std::string test = "output";
+
+    std::cout << test;
+    std::cout << test[1];
     return 0;
 }
